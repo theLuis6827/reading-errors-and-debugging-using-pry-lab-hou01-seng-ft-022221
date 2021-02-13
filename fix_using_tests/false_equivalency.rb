14fad7f96e9320a4be45209aa8f311127de8da43
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-13 09:15:19 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -15,7 +15,7 @@ def selection(num)
   if num == 1
      "YUM YUM MUNCH MUNCH MUNCH"
   elsif num == 2
-     puts "HAM HAM HAM IN MY TUMMY"
+     "HAM HAM HAM IN MY TUMMY"
   end
   #binding.pry
 end
